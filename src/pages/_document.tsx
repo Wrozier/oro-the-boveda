3ce4bdaa9407e161d2a6a08a65de35241b4efe5b
William Rozier <wrozier@princesscruises.com>
2024-08-20 16:31:42 -0400

    ui corrections

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Viewport meta tag for responsive design */}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
