d5d9fdb9f8527b0ed0b0c83d0e7506033eb13434
William Rozier <wrozier@princesscruises.com>
2024-08-24 11:40:00 -0400

    fixing navbar for each page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <title>Oro Boveda</title>
+        <meta name="description" content="Your site description here." />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
